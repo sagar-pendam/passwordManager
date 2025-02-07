989ae425850c34f3ea8066488d9e8bb5d88af3c6
pendamsagar3@gmail.com <pendamsagar3@gmail.com>
2025-02-07 04:53:38 -0800

    some bugs fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,17 +10,30 @@ import { Bounce } from "react-toastify";
 function Home() {
     
     const [form, setform] = useState({ website: "", username: "", password: "", id: "" })
-    const [passwordArray, setpasswordArray] = useState(localStorage.getItem("passwords")!=null?JSON.parse(localStorage.getItem("passwords")):[])
+    const [passwordArray, setpasswordArray] = useState([])
     const passwordRef = useRef(0)
     const usernameRef = useRef(0)
     const websiteRef = useRef(0)
     const showPassRef = useRef(0)
-
+    // console.log(passwordArray);
+    
 
     useEffect(() => {
         // console.log(passwordArray);
+        let data = localStorage.getItem("passwords")
+            if (data) {
+                let arr = JSON.parse(data)
+                setpasswordArray(arr)
+                // alert("hi")
+                // console.log("arr:",arr);
+                
+                
+
+                
+
+            }
         
-    }, [passwordArray])
+    }, [])
 
     const savePassword = () => {
        
@@ -45,17 +58,17 @@ function Home() {
             setpasswordArray([...passwordArray, form])
             let data = localStorage.getItem("passwords")
             if (data) {
-                let arr = [JSON.parse(data)]
+                let arr = JSON.parse(data)
                 arr.push(form)
                 // console.log("Local Storage:");
-                // console.log(arr);
-
+                // console.log("save arr:",arr);
+                
                 localStorage.setItem("passwords", JSON.stringify(arr))
 
             }
             else {
                 
-                localStorage.setItem("passwords", JSON.stringify(form))
+                localStorage.setItem("passwords", JSON.stringify([form]))
                 // console.log("else");
 
                 // console.log(localStorage.getItem("passwords"));
@@ -116,7 +129,7 @@ function Home() {
         // console.log(items);
 
 
-        setpasswordArray([...passwordArray.filter((item) => { item.id !== items.id })])
+        setpasswordArray([...passwordArray.filter((item) =>  item.id !== items.id )])
         setform(items)
 
     }
@@ -140,17 +153,21 @@ function Home() {
      
         if (val) {
             let data = localStorage.getItem("passwords")
-        let arr = [JSON.parse(data)]
-       let deleteItem=arr.filter((item) => { item.id !== items.id })
-       
+        let arr = JSON.parse(data)
+       let deleteItem=arr.filter((item) =>  item.id != items )
+    //    alert("handleDelete")
+    //    console.log("Delete Item:",deleteItem);
+    //    console.log(items);
        
        localStorage.setItem("passwords", JSON.stringify(deleteItem))
-
-            setpasswordArray([...passwordArray.filter((item) => { item.id !== items.id })])
+            let deleteItem2 = [...passwordArray.filter((item) =>  item.id !== items )]
+            // console.log("Delete Item2:",deleteItem2);
+            setpasswordArray(deleteItem2)
         }
     }
 
     const handleDeleteAll = ()=>{
+        alert("handleDeleteAll")
         localStorage.removeItem("passwords")
         let arr = []
         setpasswordArray([])
@@ -194,7 +211,7 @@ function Home() {
                 {/* <form className="min-w-full mx-auto flex items-center justify-center flex-col px-4"> */}
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="text" value={form.website} ref={websiteRef} onChange={(e) => handleChange(e)} name="website" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Website</label>
+                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
                     <input type="username" value={form.username} onChange={(e) => handleChange(e)} ref={usernameRef} name="username" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -288,16 +305,16 @@ function Home() {
 
                         </div>
 
-                        {passwordArray.map((item) => {
+                      {passwordArray.map((item) => {
                             return <div key={item.id} className='rows flex items-center justify-between min-w-full  px-2 py-2'>
                                 <div className='flex h-[40px] sm:min-w-[25%] min-w-[25%]  border-r-1 border-b-1 px-2 py-1 overflow-x-auto relative  text-[12px]'>
                                     <img onClick={()=>{copyData(item.website)}}  className='absolute right-0 invert cursor-pointer sm:w-[25px] w-[20px] sm:h-[25px] h-[20px]' src='./svg/copy.svg' />
-                                    <p className='overflow-x-auto pr-7 text-[12px] '>{item.website} </p>
+                                    <p  className='overflow-x-auto pr-7 text-[12px] '>{item.website} </p>
 
                                 </div>
                                 <div className='flex h-[40px] sm:min-w-[25%] min-w-[25%]  border-r-1 border-b-1 px-2 py-1 overflow-x-auto  text-[12px] relative'>
                                 <img  onClick={()=>{copyData(item.username)}} className='absolute right-0 invert cursor-pointer sm:w-[25px] w-[20px] sm:h-[25px] h-[20px]' src='./svg/copy.svg' />
-                                    <p className='overflow-x-auto pr-7 text-[12px] '>{item.username} </p>
+                                    <p  className='overflow-x-auto pr-7 text-[12px] '>{item.username} </p>
                                 </div>
                                 <div className='flex h-[40px] sm:min-w-[25%] min-w-[25%]  border-r-1 border-b-1 px-2 py-1 overflow-x-auto  text-[12px] relative'>
                                 <img  onClick={()=>{copyData(item.password)}} className='absolute right-0 invert cursor-pointer sm:w-[25px] w-[20px] sm:h-[25px] h-[20px]' src='./svg/copy.svg' />
@@ -316,7 +333,7 @@ function Home() {
                                         </lord-icon>
                                     </span>
 
-                                    <span onClick={(item) => { deleteRecord(item) }} className='cursor-pointer'>
+                                    <span onClick={() => { deleteRecord(item.id) }} className='cursor-pointer'>
                                         <lord-icon class="sm:w-[25px] sm:h-[25px] w-[20px] h-[20px]"
                                             src="https://cdn.lordicon.com/hwjcdycb.json"
                                             trigger="hover"
@@ -347,6 +364,8 @@ function Home() {
 </lord-icon>
  </button>
             </div>
+
+           
         </>
     )
 }
